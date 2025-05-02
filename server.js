4742df99e15698d6dc94d862b4f97076f1b0cd16
Greg7 <diego.gregorio@live.com>
2025-05-02 20:36:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ app.post('/api/pagamento', async (req, res) => {
     customer: {
       name: "Cliente",
       email: "cliente@email.com",
+      phone: "+5500000000000",
       document: "12345678900"
     }
   }),
