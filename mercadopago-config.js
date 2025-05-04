d4d3e184ea9952d543729dea450ff3c325f68157
Greg7 <diego.gregorio@live.com>
2025-05-04 16:36:55 -0300

    Delete mercadopago-config.js

--- mercadopago-config.js
+++
@@ -1,7 +0,0 @@
-const mercadopago = require('mercadopago');
-
-mercadopago.configure({
-  access_token: process.env.MP_ACCESS_TOKEN // Certifique-se de adicionar ao seu .env
-});
-
-module.exports = mercadopago;
